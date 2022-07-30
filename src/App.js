09e9a44afc93131591d435ed57274ae6745f3e1e
Shafak93 <shafakctg64@gmail.com>
2022-07-30 14:04:58 +0600

    counterAction, counterReducer, counterConstant

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import Counter from './Component/Counter';
 
 function App() {
   return (
     <div className="App">
-      
+      <Counter />
     </div>
   );
 }
--- src/Component/Counter.js
+++ src/Component/Counter.js
@@ -3,7 +3,9 @@ import React from 'react';
 const Counter = () => {
     return (
         <div>
-            
+            <h1>Wellcome to Redux Counter App</h1>
+            <h3>Counter : 0</h3>
+            <button>Increment</button>
         </div>
     );
 };
