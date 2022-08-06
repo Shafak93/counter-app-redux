1c81760af4d6e7643d1a2d065b306670966c0383
Shafak93 <shafakctg64@gmail.com>
2022-08-06 10:18:24 +0600

    action reducer and store functionlity added

--- src/Component/Counter.js
+++ src/Component/Counter.js
@@ -1,11 +1,26 @@
 import React from 'react';
+import { useDispatch, useSelector } from 'react-redux/es/exports';
+import { decrementCounter, incrementCounter, resetCounter } from '../services/actions/counterAction';
 
 const Counter = () => {
+    const count = useSelector(state => state.count)
+    const dispatch = useDispatch()
+    const handleIncrement = () =>{
+        dispatch(incrementCounter())
+    }
+    const handleDecrement = () =>{
+        dispatch(decrementCounter())
+    }
+    const handleReset = () =>{
+        dispatch(resetCounter())
+    }
     return (
         <div>
             <h1>Wellcome to Redux Counter App</h1>
-            <h3>Counter : 0</h3>
-            <button>Increment</button>
+            <h3>Counter : {count}</h3>
+            <button onClick={handleIncrement}>Increment</button>
+            <button onClick={handleReset}>Reset</button>
+            <button onClick={handleDecrement}>Decrement</button>
         </div>
     );
 };
